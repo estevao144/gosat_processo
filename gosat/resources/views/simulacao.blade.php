09c1a7677ef8ebbfa4f9aa084883d6447945a0b4
estevao144 <estevo144@gmail.com>
2023-04-27 14:42:04 -0300

    :sparkles:feat: views simulação e simulações criados

---
+++ gosat/resources/views/simulacao.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Simulação</title>
+</head>
+<body>
+    <h1>Simulação</h1>
+    @if( $id != null)
+    <p>Exibindo simulação do id: {{ $id }}</p>
+    @endif
+</body>
+</html>
--- gosat/routes/web.php
+++ gosat/routes/web.php
@@ -18,3 +18,72 @@ Route::get('/', function () {
 
     return view('welcome', ['nome' => $nome]);
 });
+
+Route::get('/simulacoes', function () {
+      $nome = 'Estevão';
+      $instituicoes = [
+        [
+            'id' => 1,
+            'nome' => 'Banco pingApp',
+            'modalidades' => [
+                [
+                    'nome' => 'Crédito Pessoal',
+                    'taxaJuros' => 0.02,
+                    'modalidades' => [
+                        [
+                            'nome' => 'Crédito Pessoal',
+                            'cod' => 3,
+                        ],
+                        [
+                            'nome' => 'Crédito Pessoal',
+                            'cod' => 13,
+                        ],
+                    ],
+                ],
+                [
+                    'id' => 'a50edteste',
+                    'nome' => 'Crédito Imobiliário',
+                    'taxaJuros' => 0.09,
+                    'parcelas' => [
+                        [
+                            'quantidade' => 120,
+                            'valor' => 100000,
+                        ],
+                        [
+                            'quantidade' => 180,
+                            'valor' => 200000,
+                        ],
+                        [
+                            'quantidade' => 240,
+                            'valor' => 300000,
+                        ],
+                        [
+                            'quantidade' => 300,
+                            'valor' => 400000,
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+         $simulacao = [
+                [
+                 'cpf' => '12345678910',
+                 'instituicao_id' => 1,
+                 'codModalidade' => 'a50edteste'
+                ],
+                [
+                'qntParcelaMin' => 10,
+                'qntParcelaMax' => 20,
+                'valorMin' => 1000,
+                'valorMax' => 10000,
+                'taxaJuros' => 0.5,
+                ]
+
+             ];
+    return view('simulacao', ['nome' => $nome, 'simulacao' => $simulacao, 'instituicoes' => $instituicoes]);
+});
+
+Route::get('/simulacao/{id}', function ($id = null) {
+    return view('simulacao', ['id' => $id]);
+});
